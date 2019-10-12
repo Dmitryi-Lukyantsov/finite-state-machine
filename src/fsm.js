1a81017101fbef1ac1660ef72ffb6255398afee6
Dima Lukyantsov <dmitruxa1@mail.ru>
2019-10-12 23:55:44 +0300

    2

--- src/fsm.js
+++ src/fsm.js
@@ -3,19 +3,32 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+
+        this.state = config.state,
+        this.nextState
+        this.previusState
+
+        this.arrState = []
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.state;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        this.arrState.push(this.state)
+        this.previusState = this.state;
+        this.state = state;
+    }
 
     /**
      * Changes state according to event transition rules.
@@ -26,7 +39,9 @@ class FSM {
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        return  this.state = config.initial;
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -41,7 +56,11 @@ class FSM {
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+      if (this.previusState) {
+          this.state = this.previusState;
+      }  
+    }
 
     /**
      * Goes redo to state.
@@ -53,7 +72,9 @@ class FSM {
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+        this.arrState = [];
+    }
 }
 
 module.exports = FSM;
